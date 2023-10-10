134cd08ab555a77a1323b1eacc71fa41d77bd7be
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-10 17:46:43 +0200

    Add "d-none" for ticket box and allert dfor errors

--- JS/script.js
+++ JS/script.js
@@ -4,40 +4,54 @@ const oldDiscount = 0.4;
 let submit = document.querySelector("#submit");
 
 submit.addEventListener('click',
-function(){
-    let name = document.getElementById("name").value;
-    let km = parseInt(document.getElementById("km").value);
-    let age = document.getElementById("age").value;
+    function () {
+        let name = document.getElementById("name").value;
+        let km = parseInt(document.getElementById("km").value);
+        let age = document.getElementById("age").value;
+        document.getElementById("ticket-box").classList.remove("d-none");
+        
 
-    if(km<=0 || isNaN(km)){
-        console.log('inserire numero kilometri corretto');
-    }
-    else if(age === 'young'){
-        let price = document.querySelector("#price");
-        price.innerHTML = (parseFloat((basePrice *km)*(1-youngDiscount)).toFixed(2)) + '€';
-        const offer = document.querySelector("#offer");
-        offer.innerHTML = `Young plan`;
+        if (km <= 0 || isNaN(km)) {
+            document.getElementById("ticket-allert").innerHTML = `inserire numero kilometri corretto`;
+                        document.getElementById("ticket").classList.add("d-none");
+        }
+        else if (age === 'null') {
+            document.getElementById("ticket-allert").innerHTML = `Scegli la fascia d'età`;
+                        document.getElementById("ticket").classList.add("d-none");
+        }
+        else if (name === '' || isNaN(parseInt(name))===false) {
+            document.getElementById("ticket-allert").innerHTML = `Inserire nome corretto`;
+                        document.getElementById("ticket").classList.add("d-none");
+        }
+        else {
+            document.getElementById("ticket-allert").innerHTML = `Il tuo biglietto`;
+            document.getElementById("ticket").classList.remove("d-none");
+            document.querySelector("#passenger").innerHTML = name;
+
+            if (age === 'young') {
+                let price = document.querySelector("#price");
+                price.innerHTML = (parseFloat((basePrice * km) * (1 - youngDiscount)).toFixed(2)) + '€';
+                document.querySelector("#offer").innerHTML = `Young plan`;
 
 
-    }
-    else if(age === 'old'){
-        let price =document.querySelector("#price");
-        price.innerHTML = (parseFloat((basePrice *km)*(1-oldDiscount)).toFixed(2)) + '€';
-        const offer = document.querySelector("#offer");
-        offer.innerHTML = `Over 65 plan`;
+            }
+            else if (age === 'old') {
+                let price = document.querySelector("#price");
+                price.innerHTML = (parseFloat((basePrice * km) * (1 - oldDiscount)).toFixed(2)) + '€';
+                document.querySelector("#offer").innerHTML = `Over 65 plan`;
 
 
-    }
-    else if (age === 'adult') {
-        let price =document.querySelector("#price");
-        price.innerHTML = (parseFloat(basePrice *km).toFixed(2)) + '€';
-        const offer = document.querySelector("#offer");
-        offer.innerHTML = `Basic plan`;
+
+            }
+            else if (age === 'adult') {
+                let price = document.querySelector("#price");
+                price.innerHTML = (parseFloat(basePrice * km).toFixed(2)) + '€';
+                document.querySelector("#offer").innerHTML = `Basic plan`;
 
 
-    }
-    else if(age === 'null'){
-        console.log('inserisci età');
-    }
-})
+            }
+        }
+
+
+    })
 
